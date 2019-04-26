5323ec8c4536a8e67a64a681afcf586710ed0969
konyka <konyka@126.com>
2019-04-26 10:06:33 +0800

    add main.go

---
+++ go/v001/src/lunago/main.go
@@ -0,0 +1,19 @@
+/*
+* @Author: konyka
+* @Date:   2019-04-26 10:01:20
+* @Last Modified by:   konyka
+* @Last Modified time: 2019-04-26 10:02:50
+*/
+package main
+
+func main() {
+	println("Hello world!!!")
+}
+
+
+
+
+
+
+
+
