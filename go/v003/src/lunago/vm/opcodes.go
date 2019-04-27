6049188fc79f21dcff95664659f1d1299e0bf33c
konyka <konyka@126.com>
2019-04-27 15:47:43 +0800

    add opcode const

--- go/v003/src/lunago/vm/opcodes.go
+++ go/v003/src/lunago/vm/opcodes.go
@@ -2,18 +2,72 @@
 * @Author: konyka
 * @Date:   2019-04-27 15:39:45
 * @Last Modified by:   konyka
-* @Last Modified time: 2019-04-27 15:41:03
+* @Last Modified time: 2019-04-27 15:46:40
 */
  
 package vm
 
 
+/* OpMode */
+/* basic instruction format */
 const (
-	IABC	= iota
-	IABx
-	IAsBx
-	IAx
+	IABC  = iota // [  B:9  ][  C:9  ][ A:8  ][OP:6]
+	IABx         // [      Bx:18     ][ A:8  ][OP:6]
+	IAsBx        // [     sBx:18     ][ A:8  ][OP:6]
+	IAx          // [           Ax:26        ][OP:6]
 )
+
+/* OpCode */
+const (
+	OP_MOVE = iota
+	OP_LOADK
+	OP_LOADKX
+	OP_LOADBOOL
+	OP_LOADNIL
+	OP_GETUPVAL
+	OP_GETTABUP
+	OP_GETTABLE
+	OP_SETTABUP
+	OP_SETUPVAL
+	OP_SETTABLE
+	OP_NEWTABLE
+	OP_SELF
+	OP_ADD
+	OP_SUB
+	OP_MUL
+	OP_MOD
+	OP_POW
+	OP_DIV
+	OP_IDIV
+	OP_BAND
+	OP_BOR
+	OP_BXOR
+	OP_SHL
+	OP_SHR
+	OP_UNM
+	OP_BNOT
+	OP_NOT
+	OP_LEN
+	OP_CONCAT
+	OP_JMP
+	OP_EQ
+	OP_LT
+	OP_LE
+	OP_TEST
+	OP_TESTSET
+	OP_CALL
+	OP_TAILCALL
+	OP_RETURN
+	OP_FORLOOP
+	OP_FORPREP
+	OP_TFORCALL
+	OP_TFORLOOP
+	OP_SETLIST
+	OP_CLOSURE
+	OP_VARARG
+	OP_EXTRAARG
+)
+
 
 
 
