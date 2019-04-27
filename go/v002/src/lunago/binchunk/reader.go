fbc4da082108f4a5ce0a599b5ef0baacc7a9c99a
konyka <konyka@126.com>
2019-04-27 10:02:20 +0800

    add Undump函数:用来解析二进制chunk文件的信息

---
+++ go/v002/src/lunago/binchunk/reader.go
@@ -0,0 +1,18 @@
+/*
+* @Author: konyka
+* @Date:   2019-04-27 09:51:17
+* @Last Modified by:   konyka
+* @Last Modified time: 2019-04-27 09:53:18
+*/
+
+package binchunk
+
+improt "encoding/binary"
+import "math"
+
+type reader struct {
+	data []byte
+}
+
+
+
