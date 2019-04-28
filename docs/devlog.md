9abbef622f09c216fcc8de244a1f7ac5aa039400
konyka <konyka@126.com>
2019-04-28 09:15:19 +0800

    check()方法检查栈的空闲空间是否还可以容纳（push）至少n个值，如果不满足这个条件，就会调用go的append()函数对其进行扩容.

--- docs/devlog.md
+++ docs/devlog.md
@@ -1194,6 +1194,17 @@ lua的指令，根据其作用，大致可以分为：常量加载指令、运�
     }
 
     接下来为luaStack结构体定义一些方法。
+    check()方法检查栈的空闲空间是否还可以容纳（push）至少n个值，如果不满足这个条件，就会调用go的
+    append()函数对其进行扩容。
+
+    func (self *luaStack) check(n int) {
+        free := len(self.slots) - self.top
+        for i := free; i < n; i++ {
+            self.slots = append(self.slots, nil)
+        }
+    }
+
+    
 
 
 
--- go/v004/src/lunago/state/lua_stack.go
+++ go/v004/src/lunago/state/lua_stack.go
@@ -2,7 +2,7 @@
 * @Author: konyka
 * @Date:   2019-04-27 18:15:13
 * @Last Modified by:   konyka
-* @Last Modified time: 2019-04-28 09:03:58
+* @Last Modified time: 2019-04-28 09:13:01
 */
 
 package state
@@ -12,7 +12,9 @@ type luaStack struct {
 	top		int 		//记录栈顶的索引
 }
 
-//创建指定容量的栈
+/**
+ * 创建指定容量的栈
+ */
 func newLuaStack(size int) *luaStack {
 	return &luaStack{
 		slots: make([]luaValue, size),
@@ -20,6 +22,20 @@ func newLuaStack(size int) *luaStack {
 	}
 }
 
+/**
+ * [check()方法检查栈的空闲空间是否还可以容纳（push）至少n个值，如果不满足这个条件，就会调用go的
+    append()函数对其进行扩容。]
+ * @Author   konyka
+ * @DateTime 2019-04-28T09:11:03+0800
+ * @param    {[type]}                 self *luaStack)    check(n int [description]
+ * @return   {[type]}                      [description]
+ */
+func (self *luaStack) check(n int) {
+	free := len(self.slots) - self.top
+	for i := free; i < n; i++ {
+		self.slots = append(self.slots, nil)
+	}
+}
 
 
 
