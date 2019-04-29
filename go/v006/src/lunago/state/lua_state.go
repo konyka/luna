cc9e49bbfe10cfad708a0135a0aa5f9dbf18ffe9
konyka <konyka@126.com>
2019-04-29 17:42:51 +0800

    给New函数增加了两个参数，第一个参数用于指定Lua栈的初始容量，第二个参数传入函数原型，以初始化proto字段。由于虚拟机肯定是从第一条指令开始执行的，因此pc字段初始化为0就可以了。

--- go/v006/src/lunago/state/lua_state.go
+++ go/v006/src/lunago/state/lua_state.go
@@ -2,7 +2,7 @@
 * @Author: konyka
 * @Date:   2019-04-28 11:24:28
 * @Last Modified by:   konyka
-* @Last Modified time: 2019-04-29 17:23:50
+* @Last Modified time: 2019-04-29 17:39:03
 */
 
 
@@ -21,10 +21,14 @@ type luaState struct {
 
 /**
  * 用来创建luaState的实例
+ * 给New函数增加了两个参数，第一个参数用于指定Lua栈的初始容量，第二个参数传入函数原型，以初始化proto字段。
+ * 由于虚拟机肯定是从第一条指令开始执行的，因此pc字段初始化为0就可以了。
  */
-func New() *luaState {
+func New(stackSize int, proto *binchunk.Prototype) *luaState {
     return &luaState{
-        stack: newLuaStack(20),
+        stack: newLuaStack(stackSize),
+        proto: proto,
+        pc:    0,
     }
 }
 
