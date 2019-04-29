fd8434359a251b5656c837f343d94011424af174
konyka <konyka@126.com>
2019-04-29 12:32:56 +0800

    FloatToInteger(f float64) (int64, bool)

--- go/v005/src/lunago/number/math.go
+++ go/v005/src/lunago/number/math.go
@@ -2,7 +2,7 @@
 * @Author: konyka
 * @Date:   2019-04-29 11:26:48
 * @Last Modified by:   konyka
-* @Last Modified time: 2019-04-29 11:56:32
+* @Last Modified time: 2019-04-29 12:32:13
 */
 
 package number
@@ -60,6 +60,9 @@ func ShiftLeft(a, n int64) int64 {
 
 /**
  * >> 右移
+ *  go中，如果右移运算符的左操作数是有符号整数，那么进行的就是有符号右移，空位补充1.
+ *  不过我们期望的是无符号右移，空位补充0，所以在第一个分支里面需要先将左操作数转换成无符号整数
+ *  在执行右移擦欧总，然后在将结果转换为有符号整数。如果要移动的位数小于0，则将右移转换为左移。
  */
 func ShiftRight(a, n int64) int64 {
     if n >= 0 {
@@ -69,6 +72,16 @@ func ShiftRight(a, n int64) int64 {
     }
 }
 
-
+// todo: ？？？？？correct?
+/**
+ * [Float To Integer ]
+ * @Author   konyka
+ * @DateTime 2019-04-29T12:31:57+0800
+ * @param    {[type]}                 f float64) (int64, bool [description]
+ */
+func FloatToInteger(f float64) (int64, bool) {
+    i := int64(f)
+    return i, float64(i) == f
+}
 
 
