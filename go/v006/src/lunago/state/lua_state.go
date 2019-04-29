169e986646a58ae3a54f48ec9f9994e1a1de21d2
konyka <konyka@126.com>
2019-04-29 17:25:13 +0800

    adjust type luaState struct

--- go/v006/src/lunago/state/lua_state.go
+++ go/v006/src/lunago/state/lua_state.go
@@ -2,17 +2,21 @@
 * @Author: konyka
 * @Date:   2019-04-28 11:24:28
 * @Last Modified by:   konyka
-* @Last Modified time: 2019-04-28 11:30:20
+* @Last Modified time: 2019-04-29 17:23:50
 */
 
 
 package state
+
+import "lunago/binchunk"
 /**
  * lusState状态机 结构体定义
  * 
  */
 type luaState struct {
     stack *luaStack
+    proto *binchunk.Prototype //保存函数原型
+    pc    int //程序计数器
 }
 
 /**
