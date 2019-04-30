56fb18efd46edcc84f4d6fa25e520f5f00c8abac
konyka <konyka@126.com>
2019-04-30 11:05:42 +0800

    state/lua_table.go在里面定义结构体luaTable

---
+++ go/v007/src/lunago/api/lua_vm.go
@@ -0,0 +1,27 @@
+/*
+* @Author: konyka
+* @Date:   2019-04-29 16:42:33
+* @Last Modified by:   konyka
+* @Last Modified time: 2019-04-29 16:49:12
+*/
+
+package api
+
+type LuaVM interface {
+    LuaState
+    PC() int              //返回当前PC
+    AddPC(n int)          //修改PC 用于实现跳转指令
+    Fetch() uint32        //取出当前的指令，将PC指向下一条指令
+    GetConst(idx int)     //将指定的常量push到栈顶
+    GetRK(rk int)         //将指定的常量或者栈值push到栈顶
+}
+
+
+
+
+
+
+
+
+
+
---
+++ go/v007/src/lunago/state/lua_table.go
@@ -0,0 +1,24 @@
+/*
+* @Author: konyka
+* @Date:   2019-04-30 10:55:53
+* @Last Modified by:   konyka
+* @Last Modified time: 2019-04-30 11:05:14
+*/
+
+
+package state
+
+import "math"
+import "lunago/number"
+
+type luaTable struct {
+    arr  []luaValue
+    _map map[luaValue]luaValue
+}
+
+
+
+
+
+
+
