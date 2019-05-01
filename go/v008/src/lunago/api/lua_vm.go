064eec10675b4fc95c93c36cd8291b2d21c10895
konyka <konyka@126.com>
2019-05-01 10:28:11 +0800

    update type LuaVM interface

--- go/v008/src/lunago/api/lua_vm.go
+++ go/v008/src/lunago/api/lua_vm.go
@@ -2,7 +2,7 @@
 * @Author: konyka
 * @Date:   2019-04-29 16:42:33
 * @Last Modified by:   konyka
-* @Last Modified time: 2019-04-29 16:49:12
+* @Last Modified time: 2019-05-01 10:26:59
 */
 
 package api
@@ -14,9 +14,10 @@ type LuaVM interface {
     Fetch() uint32        //取出当前的指令，将PC指向下一条指令
     GetConst(idx int)     //将指定的常量push到栈顶
     GetRK(rk int)         //将指定的常量或者栈值push到栈顶
+    RegisterCount() int     //计数器
+    LoadVararg(n int)       //加载vararg
+    LoadProto(idx int)      //加载原型
 }
-
-
 
 
 
